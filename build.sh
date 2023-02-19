ebcfaa0a5be0b8aef12acb7e36b0f70bfcb126c4
PandaNinjas <admin@malwarefight.gq>
2023-02-19 07:44:17 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ pushd build || exit
 
 # download bubblewrap
 git clone https://github.com/containers/bubblewrap.git
-bubblewrap_version="$(cd bubblewrap && git describe --long --tags|sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')"
+bubblewrap_version="$(cd bubblewrap && git describe --always)"
 echo "BWRAP_VER=${bubblewrap_version}_$(date +%s)" >> $GITHUB_ENV
 mv bubblewrap "bubblewrap-${bubblewrap_version}"
 echo "= downloading bubblewrap v${bubblewrap_version}"
