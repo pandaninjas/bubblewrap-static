a66399d5498f1be8284ab6660cb35f06dec36af8
PandaNinjas <admin@malwarefight.wip.la>
2023-04-09 18:56:11 -0700

    Update should_rebuild.sh

--- should_rebuild.sh
+++ should_rebuild.sh
@@ -5,8 +5,8 @@ cd $(dirname "$0") || exit
 bubblewrapdate=$(git log -1 --format="%at")
 curdate=$(date +%s)
 ((curdate-=86400))
-
-if [[ curdate -lt bubblewrapdate ]]; then
+echo $bubblewrapdate $curdate
+if [[ $curdate -lt $bubblewrapdate ]]; then
   exit 0
 fi
 exit 1
