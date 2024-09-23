f2ada3e055159740eef5db5a52abb11e90b6ebc1
pandaninjas <admin@malwarefight.wip.la>
2024-09-22 19:43:41 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ echo "= downloading bubblewrap v${bubblewrap_version}"
 
 echo "= building bubblewrap"
 pushd bubblewrap-${bubblewrap_version}
-git am < 0001-make-deps-static.patch
+git am < $(dirname $(readlink -f $0))/0001-make-deps-static.patch
 meson --buildtype=plain release -Dc_args="-static -g -O2 -Os -ffunction-sections -fdata-sections" -Dc_link_args='--static'
 meson compile -C release
 popd # bubblewrap-${bubblewrap_version}
